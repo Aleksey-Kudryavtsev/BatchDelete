e84293aa9c48ff63bbf46a518711e142afc491d2
Aleksey Kudryavtsev <aleksey.kudryavtsev@gmail.com>
2012-10-22 15:13:33 +0300

    Document library support + Recycle bin support
    Also Remove Recursion due to problems with batch deleting nested
    folders, batch deleting top level folders successfully deletes nested
    folders provided the folders are empty. Probably the "recursive" mode
    should be used to delete nested files first and then folders should be
    deleted in a second step

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Web;
 using Microsoft.SharePoint;
 
 namespace BatchDelete
 {
     class Program
     {
+        private const int RootWebUrlArgumentIndex = 2;
         private const int SiteUrlArgumentIndex = 1;
         private const int BatchSizeArgumentIndex = 0;
 
@@ -14,7 +17,7 @@ namespace BatchDelete
 
         static void Main(string[] args)
         {
-            if(args.Length < 3)
+            if(args.Length < 2)
             {
                 PrintUsage();
                 return;
@@ -23,20 +26,72 @@ namespace BatchDelete
             if(!ReadBatchSizeFromCommandLine(args))
             {
                 PrintUsage();
-                Console.WriteLine("Batch size should be an positive integer");
+                Console.WriteLine("Batch size should be a positive integer");
                 return;
             }
 
-            using(SPSite site = new SPSite(args[SiteUrlArgumentIndex]))
+            using (SPSite site = new SPSite(args[SiteUrlArgumentIndex]))
             {
-                for (int i = SiteUrlArgumentIndex + 1; i < args.Length; i++)
+                if (args[RootWebUrlArgumentIndex] == "RecycleBin")
+                {
+                    CleanRecycleBin(site);
+                }
+                else
+                {
+                    CleanSites(args, site);
+                }
+            }
+        }
+
+        private static void CleanRecycleBin(SPSite site)
+        {
+            Console.WriteLine(string.Format("Site recycle bin contains {0} items.", site.RecycleBin.Count));
+
+            while (site.RecycleBin.Count > 0)
+            {
+                SPRecycleBinItemCollection recycleBin = site.RecycleBin;
+
+                Console.WriteLine(string.Format("Starting a new batch. Current item count: {0}. Batch size: {1}", recycleBin.Count, BatchSize));
+
+                List<Guid> recycleBinItemsBatch = new List<Guid>();
+                for (int i = 0; i < BatchSize && i < recycleBin.Count; i++)
+                {
+                    recycleBinItemsBatch.Add(recycleBin[i].ID);
+                }
+
+                recycleBin.Delete(recycleBinItemsBatch.ToArray());
+                Console.WriteLine("Batch completed");
+
+            }
+        }
+
+        private static void CleanSites(string[] args, SPSite site)
+        {
+            using (SPWeb web = GetSPWeb(args, site))
+            {
+                for (int i = RootWebUrlArgumentIndex + 1; i < args.Length; i++)
                 {
                     string listUrl = args[i];
                     Console.WriteLine(string.Format("Cleaning list: {0}", listUrl));
-                    SPList list = site.RootWeb.GetList(listUrl);
-
+                    SPList list = web.GetList(listUrl);
                     CleanList(list);
                 }
+            }
+        }
+
+
+        private static SPWeb GetSPWeb(string[] args, SPSite site)
+        {
+            string rootWebUrl = args[RootWebUrlArgumentIndex];
+            SPWeb web;
+
+            if (rootWebUrl == "root")
+            {
+                return site.RootWeb;
+            }
+            else
+            {
+                return site.OpenWeb(rootWebUrl);
             }
         }
 
@@ -58,7 +113,7 @@ namespace BatchDelete
 
         private static void PrintUsage()
         {
-            Console.WriteLine("usage: BatchDelete <batch_size> <site url> <list1 url> ... <listN url>");
+            Console.WriteLine("usage: BatchDelete <batch_size> <site url> (RecycleBin | <subwebUrl> <list1 url> ... <listN url>)");
         }
 
         private static void CleanList(SPList list)
@@ -97,7 +152,6 @@ namespace BatchDelete
             SPQuery spQuery = new SPQuery();
             spQuery.Query = "";
             spQuery.ViewFields = "";
-            spQuery.ViewAttributes = "Scope=\"RecursiveAll\"";
             spQuery.RowLimit = BatchSize;
             return spQuery;
         }
@@ -108,7 +162,19 @@ namespace BatchDelete
             try
             {
                 list.ParentWeb.AllowUnsafeUpdates = true;
-                list.ParentWeb.ProcessBatchData(batchDeleteXmlCommand);
+
+                Console.WriteLine(batchDeleteXmlCommand);
+
+                Console.ReadKey(true);
+
+                string result = list.ParentWeb.ProcessBatchData(batchDeleteXmlCommand);
+
+                if(result != null)
+                {
+                    Console.WriteLine(result);
+                }
+
+                Console.ReadKey(true);
             }
             finally
             {
@@ -120,13 +186,25 @@ namespace BatchDelete
         {
             StringBuilder xmlCommandStringBuilder = new StringBuilder();
             xmlCommandStringBuilder.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Batch>");
-            string command = "<Method><SetList Scope=\"Request\">" + list.ID +
-                             "</SetList><SetVar Name=\"ID\">{0}</SetVar><SetVar Name=\"Cmd\">Delete</SetVar></Method>";
+
+
+            StringBuilder commandFormatBuilder = new StringBuilder();
+
+            commandFormatBuilder.Append("<Method>");
+            commandFormatBuilder.Append("<SetList Scope=\"Request\">" + list.ID + "</SetList>");
+            commandFormatBuilder.Append("<SetVar Name=\"ID\">{0}</SetVar>");
+            commandFormatBuilder.Append("<SetVar Name=\"owsfileref\">{1}</SetVar>");
+            commandFormatBuilder.Append("<SetVar Name=\"Cmd\">Delete</SetVar>");
+            commandFormatBuilder.Append("</Method>");
+
+            string commandFormat = commandFormatBuilder.ToString();
 
             foreach (SPListItem item in items)
             {
-                xmlCommandStringBuilder.Append(string.Format(command, item.ID.ToString()));
+                xmlCommandStringBuilder.Append(string.Format(commandFormat, item.ID.ToString(), HttpUtility.UrlDecode((string)item[SPBuiltInFieldId.EncodedAbsUrl])));
             }
+
+
             xmlCommandStringBuilder.Append("</Batch>");
             return xmlCommandStringBuilder.ToString();
         }
